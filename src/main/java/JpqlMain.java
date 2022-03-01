760c3fa32aec2ace67025e0e767ca79a11d543fd
DeveloperRooney <han_0427@naver.com>
2022-03-02 01:42:30 +0900

    Entity와 Entity의 id 값으로 조회했을 때의 결과는 같다

--- src/main/java/JpqlMain.java
+++ src/main/java/JpqlMain.java
@@ -42,20 +42,13 @@ public class JpqlMain {
             em.flush();
             em.clear();
 
-            // fetch join 대상에는 웬만하면 별칭을 주면 안 된다(관례)
-            // 문제가 발생할 수 있음, 또한 JPA에서는 객체 그래프는 모두 조회하도록 설계됨
-            // 둘 이상의 컬렉션은 페치 조인을 할 수 없다
-            // 컬렉션을 페치 조인하면 페이징 API를 사용할 수 없다
-            String query = "select t from Team t join fetch t.members m";
+            String query = "select m from Member m where m.team = :team";
 
-            List<Team> members = em.createQuery(query, Team.class).getResultList();
+            Member findMember = em.createQuery(query, Member.class)
+                    .setParameter("team", teamB)
+                    .getSingleResult();
 
-            for (Team team : members) {
-                System.out.println("team = " + team.getName() + " | Member Size : " + team.getMembers().size());
-                for (Member member : team.getMembers()) {
-                    System.out.println("--> Member = " + member.getUserName());
-                }
-            }
+            System.out.println("Member Name : " + findMember.getUserName());
 
             tx.commit();
         }catch (Exception e) {
