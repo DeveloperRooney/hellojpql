c9b17800b11693d57c65e17eabc095541959928b
DeveloperRooney <han_0427@naver.com>
2022-03-01 14:49:37 +0900

    JPQL에서 SQL의 CASE 조건식 사용하는 법 학습

--- src/main/java/JpqlMain.java
+++ src/main/java/JpqlMain.java
@@ -1,9 +1,7 @@
 import jpql.Member;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
-import javax.persistence.Persistence;
+import javax.persistence.*;
+import java.util.List;
 
 public class JpqlMain {
 
@@ -13,11 +11,24 @@ public class JpqlMain {
         EntityManager em = emf.createEntityManager();
         EntityTransaction tx = em.getTransaction();
 
+        tx.begin();
+
         try {
             Member member = new Member();
             member.setUserName("hancoding");
             member.setAge(28);
+            em.persist(member);
 
+            String query = "select " +
+                    "case when m.age <= 10 then '학생 요금' " +
+                    "when m.age >= 60 then '경로요금' " +
+                    "else '일반요금' end " +
+                    "from Member m";
+            List<String> resultList = em.createQuery(query, String.class).getResultList();
+
+            for (String s : resultList) {
+                System.out.println("=========" + s + "============");
+            }
             tx.commit();
         }catch (Exception e) {
             tx.rollback();
